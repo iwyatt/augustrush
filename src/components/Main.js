27d5093260251d0cd4eb46a11880c04dff7c0432
Isaac J Wyatt <wyatt@Isaacs-MacBook-Pro.local>
2018-02-19 17:32:14 -0800

    Fix RSVP Form Attempt Two

--- src/components/Main.js
+++ src/components/Main.js
@@ -27,13 +27,13 @@ class Main extends React.Component {
   }
 
   render() {
-    let thanks =
+    let thanks =(
         <div>
         <h2 className="major">Thank you!</h2>
         <p>You should receive an email confirming we received your details.</p>
         </div>
-
-    let form =
+    )
+    let form =(
         <div>
     <h2 className="major">Send details</h2>
     <p>Provide your contact information and we will send you the details and an RSVP this summer.</p>
@@ -76,11 +76,10 @@ class Main extends React.Component {
       </ul>
       </form>
     </div>
-
+    )
     let close = <div className="close" onClick={() => {this.props.onCloseArticle()}}></div>
     return (
       <div id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
-
         <article id="thankyou" className={`${this.props.article === 'thankyou' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Thank You</h2>
           <span className="image main"><img src={pic01} alt="" /></span>
@@ -91,7 +90,6 @@ class Main extends React.Component {
           </ul>
           {close}
         </article>
-
         <article id="faq" className={`${this.props.article === 'faq' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Information</h2>
           <span className="image main"><img src={ladyoflavang} alt="" /></span>
@@ -99,13 +97,12 @@ class Main extends React.Component {
           <h2 className="major">Day's Events</h2>
           <h3>Asking Ceremony</h3>
           <p>One traditional Vietnamese pre-wedding ceremony is called Lễ Đính Hôn (also referred to as Đám Hỏi, or literally translated “Asking Ceremony”), which is where the groom and his family ask the bride’s family for their daughter’s hand in marriage. Usually attended only my family and close friends.</p>
-          <p>The general order of events is as follows:
+          <p>The general order of events is as follows:</p>
           <ul>
           <li>The groom and his family and friends hold a procession to the bride’s house bearing gifts</li>
           <li>With blessings from both families, the groom will officially ask for the bride’s hand. Gifts and a tea ceremony with the parents will ensue. </li>
           <li>Guests are invited to lunch at the house where they will enjoy what the bride’s family has cooked, as well as, the roast pig and the sticky rice that the groom and his family has brought</li>
           </ul>
-          </p>
           <p>Men generally wear suit and tie, and women wear toned-down cocktail attire or traditional Vietnamese ao dai</p>
           <h3>Wedding Mass</h3>
           <p>The wedding ceremony will be presented in both Vietnamese and English.</p>
@@ -116,25 +113,21 @@ class Main extends React.Component {
           <p>Cocktail attire is fine for both men and women but come wearing whatever you want; just be ready to join in our love and joy!</p>
           {close}
         </article>
-
         <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">About</h2>
           <span className="image main"><img src={pic03} alt="" /></span>
           <p>Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.</p>
           {close}
         </article>
-
         <article id="OurStory" className={`${this.props.article === 'OurStory' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">First Sight</h2>
           <p>In 2008, Yen and Isaac met at a Santa Barbara coffee shop, then known as the 'North Star'. Whether it was Yen or Isaac who approached the other first is a questions of semantics - you may have to listen to both versions of the story in order to decide.</p>
           <p>They quickly hit it off discussing the books they were reading (<i>Catch-22</i> by Joseph Heller and <i>We Were the Mulvaneys</i> by Joyce Carol Oates), and sipping coffee and tea. For the remainder of the day, Isaac showed Yen around downtown Santa Barbara.</p>
           <span className="image main"><img src={picourstory} alt="" /></span>
-
           <h2 className="major">Adventures</h2>
           <p>Since then, Yen and Isaac have traversed parts of the world and shared many adventures together.</p>
           <span className="image main"><img src={adventures} alt="" /></span>
           <p>They have been on numerous motorcycle rides, camping trips, and hikes throughout the world - visiting breweries, restaurants, and friends' weddings too!</p>
-
           <h2 className="major">The Proposal</h2>
           <p>In September 2017, after the 5th attempt at winning a lottery pass to climb Half Dome in Yosemite National Park, Isaac took Yen out for an motorcycle adventure to Yosemite. After a night spent at a roadside motel, they awoke early to begin a eight hour trek to summit Half Dome and back without knowing when or where they would spend the night.</p>
           <p>Upon summiting Half Dome, and a chance meeting with Isaac's friend Viren, Isaac asked Yen to pose for a photo with him on a ledge known as 'The Visor' and Viren to take the picture. Neither Yen nor Viren knew what was about to happen. It was here that Isaac put Yen between a rock and a hard place.</p>
